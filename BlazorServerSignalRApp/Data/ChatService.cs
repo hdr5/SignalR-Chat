ae29e596e4308698d2d838a02eac9e007fe1bb78
hdr5 <hdr54200@gmail.com>
2023-01-13 03:19:13 +0200

    tiny problem

---
+++ BlazorServerSignalRApp/Data/ChatService.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorServerSignalRApp.Data
+{
+    public class ChatService
+    {
+        public Dictionary<string, string> Users;
+
+        public ChatService()
+        {
+            Users = new Dictionary<string, string>();
+        }
+
+        public void AddUser(string id,string name)
+        {
+            try {
+            Users.Add(id,name); 
+            Console.WriteLine("Id:"+id +"Name:"+name + " added");
+            }
+            catch {
+                Console.WriteLine("Error: The user has already been added");
+            }
+        }
+    }
+}
---
+++ BlazorServerSignalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using BlazorServerSignalRApp.Data;
+
+
+namespace BlazorServerSignalRApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatService _service;
+
+        public ChatHub(ChatService service)
+        {
+            _service = service;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", _service.Users.FirstOrDefault(u => u.Key == Context.ConnectionId), message);
+        }
+
+        public async Task SendPrivateMessage(string toUser, string message)
+        {
+            await Clients.Client(toUser).SendAsync("ReceiveMessage", _service.Users.FirstOrDefault(u => u.Key == Context.ConnectionId), message);
+        }
+
+
+    }
+}
